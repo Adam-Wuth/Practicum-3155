a3d3439406e1d4317e0cf3a1e5af65ffff04d05c
Adam Wuth <adwu8800@colorado.edu>
2024-08-06 11:38:28 -0600

    Continued hierarchy

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,5 +1,8 @@
 //**TODO: Your code here**
+//Extends expr
 sealed trait Expr
+
+sealed trait Value extends Expr
 
 case class ManyExprs(lst: List[Expr]) extends Expr 
 
@@ -7,7 +10,22 @@ case class Plus(e1: Expr, e2: Expr) extends Expr
 
 case class Not(e: Expr) extends Expr
 
-case class Count(e: Expr, expr2: Expr) extends Expr
+case class Count(e1: Expr, e2: Expr) extends Expr
+
+//Extends value
+
+case object Cry extends Value
+
+case object Happy extends Value
+
+case object VeryHappy extends Value
+
+case object Sleepy extends Value
+
+case object Stun extends Value
+
+case class ManyVals(v : List[Value])
+
 
 
 
