7e6097e78ac862172f58be770abe08cbb5e06534
Adam Wuth <adwu8800@colorado.edu>
2024-08-06 11:30:38 -0600

    started working on the heirarchy

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,1 +1,17 @@
 //**TODO: Your code here**
+sealed trait Expr
+
+case class ManyExprs(lst: List[Expr]) extends Expr 
+
+case class Plus(e1: Expr, e2: Expr) extends Expr
+
+case class Not(e: Expr) extends Expr
+
+case class Count(e: Expr, expr2: Expr) extends Expr
+
+
+
+
+
+
+
